9e8b1ec75546af3e1eddde6a69603d571266e717
SquallStrifeX <squallstrifex@gmail.com>
2023-11-28 16:14:07 +0100

    end milestone 1

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+
+function palindromaQuestion(parola){
+    let parolaMinuscola = parola.toLowerCase();
+
+    let parolaInversa = parolaMinuscola.split('').reverse().join('');
+
+    return parolaMinuscola === parolaInversa
+}
+
+
+let parola = prompt("Inserisci una Parola");
+
+let risultato = palindromaQuestion(parola);
+
+if(risultato == true){
+    console.log("La parola è palindroma");
+}
+
+else{
+    console.log("La parola non è palindroma")
+}
